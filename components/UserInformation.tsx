34c5a1f65cfc6c8e56e6162c225fd9a375e7310d
sohailshams <sohailshamas@yahoo.com>
2024-10-29 23:49:14 +0000

    Added user info whilst signed in and signed out.

--- components/UserInformation.tsx
+++ components/UserInformation.tsx
@@ -1,10 +1,13 @@
 import { currentUser } from "@clerk/nextjs/server";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
+import { Button } from "./ui/button";
 
 async function UserInformation() {
   const user = await currentUser();
+  console.log(user);
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center bg-white mr-6 rounded-lg py-4">
       <Avatar>
         {user?.id ? (
           <AvatarImage src={user?.imageUrl} />
@@ -16,6 +19,35 @@ async function UserInformation() {
           {user?.lastName?.charAt(0)}
         </AvatarFallback>
       </Avatar>
+
+      <SignedIn>
+        <div className="text-center">
+          <p className="font-semibold">
+            {user?.firstName} {user?.lastName}
+          </p>
+        </div>
+      </SignedIn>
+
+      <SignedOut>
+        <div className="text-center space-y-2">
+          <p className="font-semibold">You are not signed in.</p>
+          <Button asChild className="bg-[#0B63C4] text-white">
+            <SignInButton>Sign in</SignInButton>
+          </Button>
+        </div>
+      </SignedOut>
+
+      <hr className="w-full border-gray-200 my-5" />
+
+      <div className="flex justify-between w-full px-4 text-sm">
+        <p className="font-semibold text-gray-400">Posts</p>
+        <p className="text-blue-400">0</p>
+      </div>
+
+      <div className="flex justify-between w-full px-4 text-sm">
+        <p className="font-semibold text-gray-400">Comments</p>
+        <p className="text-blue-400">0</p>
+      </div>
     </div>
   );
 }
