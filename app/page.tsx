fcc760fe82f95d67067ad21973337b5c38aa3792
sohailshams <sohailshamas@yahoo.com>
2024-10-31 17:47:48 +0000

    Added text input and image input.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import PostForm from "@/components/PostForm";
-import { Button } from "@/components/ui/button";
 import UserInformation from "@/components/UserInformation";
-import Image from "next/image";
 
 export default function Home() {
   return (
--- components/PostForm.tsx
+++ components/PostForm.tsx
@@ -1,16 +1,79 @@
 "use client";
 import { useUser } from "@clerk/nextjs";
 import UserAvatar from "./UserAvatar";
+import { Button } from "./ui/button";
+import { ImageIcon, XIcon } from "lucide-react";
+import React, { useRef, useState } from "react";
 
 function PostForm() {
+  const ref = useRef<HTMLFormElement>(null);
+  const inputFileRef = useRef<HTMLInputElement>(null);
+  const [preview, setPreview] = useState<string | null>(null);
   const { user } = useUser();
+  const handleImageChage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const imageFile = event.target.files?.[0];
+    if (imageFile) {
+      setPreview(URL.createObjectURL(imageFile));
+    }
+  };
+  const handleRemoveImage = () => {
+    setPreview(null);
+    if (inputFileRef.current) {
+      inputFileRef.current.value = ""; // Reset file input value
+    }
+  };
+
   return (
-    <div>
-      <form action="">
-        <div>
+    <div className="mb-2">
+      <form ref={ref} action="" className="p-3 bg-white rounded-lg">
+        <div className="flex items-center space-x-2">
           <UserAvatar user={user} />
+          <input
+            type="text"
+            name="postInput"
+            placeholder="Start a post..."
+            className="flex-1 rounded-full py-3 px-4 border outline-none"
+          />
+          <input
+            ref={inputFileRef}
+            type="file"
+            name="postImage"
+            accept="image/*"
+            hidden
+            onChange={handleImageChage}
+          />
+          <button type="submit" hidden>
+            Post
+          </button>
+        </div>
+
+        {preview && (
+          <div className="mt3">
+            <img className="w-full object-cover" src={preview} alt="preview" />
+          </div>
+        )}
+        <div className="flex justify-end space-x-2 mt-2">
+          <Button
+            type="button"
+            onClick={() => inputFileRef.current?.click()}
+            className="bg-[#0a66c2]"
+          >
+            <ImageIcon className="mr-2" size={16} color="currentColor" />
+            {preview ? "Change" : "Add"} Image
+          </Button>
+          {preview && (
+            <Button
+              type="button"
+              variant="destructive"
+              onClick={handleRemoveImage}
+            >
+              <XIcon className="mr-2" size={16} color="currentColor" />
+              Remove Image
+            </Button>
+          )}
         </div>
       </form>
+      <hr className="border-gray-300 mt-2" />
     </div>
   );
 }
