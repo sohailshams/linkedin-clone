a2cef767aab0e7937d0725abed6d4833d7f3aed9
sohailshams <sohailshamas@yahoo.com>
2024-11-07 23:41:14 +0000

    Added condition to not display posts if user is not logged in.

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,24 @@
 import PostForm from "@/components/PostForm";
 import UserInformation from "@/components/UserInformation";
+import { currentUser } from "@clerk/nextjs/server";
 
-export default function Home() {
+export default async function Home() {
+  const user = await currentUser();
   return (
     <div className="grid grid-cols-8 mt-5 sm:px-5">
       <section className="hidden md:inline md:col-span-2">
         <UserInformation />
       </section>
+      {user && (
+        <>
+          <section className="col-span-full md:col-span-6 xl:col-span-4 xl:max-w-xl mx-auto w-full">
+            <PostForm />
+            {/* post feed */}
+          </section>
 
-      <section className="col-span-full md:col-span-6 xl:col-span-4 xl:max-w-xl mx-auto w-full">
-        <PostForm />
-        {/* post feed */}
-      </section>
-
-      <section>{/* widget */}</section>
+          <section>{/* widget */}</section>
+        </>
+      )}
     </div>
   );
 }
